06508e492973a844c2fd98296a7a97fec0b7ab58
Jozef Chraplewski <jozef@vzaar.com>
2014-12-29 17:46:06 +0100

    wip

--- examples/process_video_spec.js
+++ examples/process_video_spec.js
@@ -10,13 +10,12 @@ describe("ProcessVideo", function(){
     var login = helper.getConf("login"),
         token = helper.getConf("token"),
         path = "./examples/video.mp4",
-        api = new Vzaar.Api({ login: login, token: token });
+        api = new Vzaar.Api({ login: login, token: token, hostname: "app.vzaar.localhost", forceHttp: true });
        
 
     it("returns 200", function(done){
       api.s3Upload(path, function(statusCode, data){
         api.processVideo(function(statusCode, _data){
-          debugger
           assert.equal(statusCode, 200);
         }, { guid: data.guid, profile: 3, title: "nodejs-test" });
       });
--- examples/video_details_spec.js
+++ examples/video_details_spec.js
@@ -7,18 +7,20 @@ describe("VideoDetails", function(){
   describe("Authorised", function(){
     this.timeout(10000);
 
-    var login = helper.getConf("login"),
-        token = helper.getConf("token"),
-        videoId = helper.getConf("videoId"),
-        api = new Vzaar.Api({ login: login, token: token });
+    var api = new Vzaar.Api({ login: helper.getConf("login"),
+                              token: helper.getConf("token"),
+                              hostname: helper.getConf("hostname"),
+                              forceHttp: helper.getConf("forceHttp") }),
+
+        videoId = helper.getConf("videoId");
     
     
-    it("returns video details", function(done){
-      api.videoDetails(videoId, function(statusCode, data){
-        assert.equal(data.author_name, login);
-        done();
-      });
-    });
+//    it("returns video details", function(done){
+//      api.videoDetails(videoId, function(statusCode, data){
+//        assert.equal(data.author_name, helper.getConf("login"));
+//        done();
+//      });
+//    });
 
     it("returns 200", function(done){
       api.videoDetails(videoId, function(statusCode, data){        
