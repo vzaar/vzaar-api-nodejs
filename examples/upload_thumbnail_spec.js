f7d842d817d9dd7583aa1a4b219fd8b0d8f5b464
Jozef Chraplewski <jozef@vzaar.com>
2014-12-30 14:11:39 +0100

    multipart wip

--- examples/upload_thumbnail_spec.js
+++ examples/upload_thumbnail_spec.js
@@ -3,5 +3,15 @@ var assert = require("assert"),
 
 describe("UploadThumbnail", function(){
   describe("Authorised", function(){
+    this.timeout(35000);
+    var api = helper.init(),
+        videoId = helper.getConf("videoId"),
+        path = "./examples/pic.jpg";
+    
+    it("returns 200", function(done){
+      api.uploadThumbnail(videoId, function(statusCode, data){
+        
+      }, { path: path});
+    });
   });
 });
