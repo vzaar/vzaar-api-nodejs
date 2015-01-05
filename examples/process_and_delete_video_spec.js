f3f8c9e6f6d9f9ef8e41ed2025ffb19eff63ad51
Jozef Chraplewski <jozef@vzaar.com>
2015-01-05 14:05:06 +0100

    uploadAndProcessVideo spec added

--- examples/process_and_delete_video_spec.js
+++ examples/process_and_delete_video_spec.js
@@ -2,20 +2,32 @@ var assert = require("assert"),
     helper = require("./helper");
 
 describe("ProcessAndDeleteVideo", function(){
+  var path = "./examples/video.mp4";
   describe("Authorised", function(){
     this.timeout(35000);
-    var api = helper.init(),
-        path = "./examples/video.mp4";
-       
-    it("returns 200", function(done){
+    var api = helper.init();
+    
+    it("returns 200", function(done){      
       api.uploadAndProcessVideo(path, function(statusCode, data){
         assert.equal(statusCode, 200);
-
+    
         // clean up
-        api.deleteVideo(_data.id, function(statusCode, __data){
+        api.deleteVideo(data.id, function(statusCode, _data){
           assert.equal(statusCode, 200);
           done();
         });
+      }, { profile: 3, title: "nodejs-test" });
+    });
+  });
+
+  describe("Unauthorised", function(){
+    this.timeout(35000);
+    var api = helper.init({token: "invalid"});
+    it("returns 401", function(done){
+      api.uploadAndProcessVideo(path, function(statusCode, data){
+        assert.equal(statusCode, 401);
+        assert.equal(data["vzaar-api"]["error"]["type"], "Not Authorized");
+        done();
       }, { profile: 3, title: "nodejs-test" });
     });
   });
