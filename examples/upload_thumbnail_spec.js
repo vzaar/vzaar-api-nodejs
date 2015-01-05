3d9381ad87fe5d6f68e1d166e808cdd32deff7b7
Jozef Chraplewski <jozef@vzaar.com>
2015-01-05 09:03:07 +0100

    upload thumbnail fixed

--- examples/upload_thumbnail_spec.js
+++ examples/upload_thumbnail_spec.js
@@ -8,9 +8,11 @@ describe("UploadThumbnail", function(){
         videoId = helper.getConf("videoId"),
         path = "./examples/pic.jpg";
     
-    it("returns 200", function(done){
+    it("returns 202", function(done){
       api.uploadThumbnail(videoId, function(statusCode, data){
-        
+        assert.equal(statusCode, 202);
+        assert.equal(data["vzaar-api"]["status"], "Accepted");
+        done();
       }, { path: path});
     });
   });
