4afc0f1cfba90a9b789fbe941fd545d346175cc4
Jozef Chraplewski <jozef@vzaar.com>
2015-01-05 17:14:59 +0100

    tests for non-authorised reqests added

--- examples/helper.js
+++ examples/helper.js
@@ -10,8 +10,8 @@ module.exports = {
   init: function(_params){
     var params = _params || {},
         api = new Vzaar.Api({
-          login: params.login || this.getConf("login"),
-          token: params.token || this.getConf("token"),
+          login: (params.login === undefined ? this.getConf("login") : params.login),
+          token: (params.token === undefined ? this.getConf("token") : params.token),
           hostname: params.hostname || this.getConf("hostname"),
           forceHttp: params.forceHttp || this.getConf("forceHttp")
         });
--- examples/video_details_spec.js
+++ examples/video_details_spec.js
@@ -16,11 +16,37 @@ describe("VideoDetails", function(){
         done();
       });
     });
-
+    
     it("returns 200", function(done){
       api.videoDetails(videoId, function(statusCode, data){        
         assert.equal(statusCode, 200);
         done();
+      });
+    });
+  });
+
+  describe("Unuthorised", function(){
+    describe("protected api", function(){
+      var api = helper.init({login: "", token: ""}),
+          videoId = helper.getConf("videoId");
+
+      it("returns 401", function(done){
+        api.videoDetails(videoId, function(statusCode, data){
+          assert.equal(statusCode, 401);
+          done();
+        });
+      });
+    });
+    
+    describe("public api", function(){
+      var api = helper.init({login: "", token: ""}),
+          videoId = helper.getConf("publicVideoId");
+
+      it("returns video details", function(done){
+        api.videoDetails(videoId, function(statusCode, data){
+          assert.equal(statusCode, 200);
+          done();
+        });
       });
     });
   });
